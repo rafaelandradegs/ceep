342ec8dedee9d6bc1930a890a686dfc65785e41a
Rafael Andrade Goncalves <rafgon@cpqd.com.br>
2022-04-07 15:09:56 -0300

    Adicionando estado local

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,6 +6,15 @@ class FormularioCadastro extends Component {
     this.titulo = ''
     this.texto = ''
     this.categoria = 'Sem categoria'
+    this.state = { categorias: [] }
+  }
+
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this))
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({ ...this.state, categorias })
   }
 
   _handleMudancaCategoria(evento) {
@@ -37,7 +46,7 @@ class FormularioCadastro extends Component {
           onChange={this._handleMudancaCategoria.bind(this)}
         >
           <option>Sem Categoria</option>
-          {this.props.categorias.map((categoria, index) => {
+          {this.state.categorias.map((categoria, index) => {
             return <option key={index}>{categoria}</option>
           })}
         </select>
