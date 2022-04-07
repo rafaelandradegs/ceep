2bfb5936b004b9801796641eb72de1f1b1a155dc
Rafael Andrade Goncalves <rafgon@cpqd.com.br>
2022-04-07 14:48:40 -0300

    Ciclo de vidas do Objetos

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
-import "./estilo.css";
+import React, { Component } from 'react'
+import './estilo.css'
 class ListaDeCategorias extends Component {
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this))
+  }
+
+  _novasCategorias(categorias) {
+    console.log(categorias)
+  }
+
   _handleEventoInput(e) {
-    if (e.key == "Enter") {
-      let valorCategoria = e.target.value;
-      this.props.adicionarCategoria(valorCategoria);
+    if (e.key == 'Enter') {
+      let valorCategoria = e.target.value
+      this.props.adicionarCategoria(valorCategoria)
     }
   }
+
   render() {
     return (
       <section className="lista-categorias">
         <ul className="lista-categorias_lista">
-          {this.props.categorias.map((categoria, index) => {
+          {this.props.categorias.categorias.map((categoria, index) => {
             return (
               <li key={index} className="lista-categorias_item">
                 {categoria}
               </li>
-            );
+            )
           })}
         </ul>
         <input
@@ -26,8 +35,8 @@ class ListaDeCategorias extends Component {
           onKeyUp={this._handleEventoInput.bind(this)}
         />
       </section>
-    );
+    )
   }
 }
 
-export default ListaDeCategorias;
+export default ListaDeCategorias
